c88873bde2486f8df7eef4c899210c3ea8bd291b
DemoraTenshi <croq45@gmail.com>
2024-11-04 12:26:59 +0100

    import correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import About from './pages/about/About'
+import Error404 from './pages/error404/Error404'
 import Home from './pages/home/Home'
 import Housing from './pages/housing/Housing'
-import Error404 from './pages/error404/Error404'
-import About from './pages/about/About'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
 
 function App() {
-    return (
-        <Router>
-            <div className='main__container'>
-                <Header />
-                <main className='main__content'>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/Housing/:id' element={<Housing />} />
-                        <Route path='/About' element={<About />} />
-                        <Route path='*' element={<Error404 />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="main__container">
+        <Header />
+        <main className="main__content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Housing/:id" element={<Housing />} />
+            <Route path="/About" element={<About />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App
