736dbecadecbeb2987693220b8536233b5d83d47
DemoraTenshi <croq45@gmail.com>
2024-11-04 14:47:19 +0100

    Fix Housing.jsx

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -6,10 +6,15 @@ const Card = ({ data }) => {
   const { id, cover, title } = data
 
   return (
-    <Link to={`/housing/${id}`} className='card__link-housing'>
-      <figure className='gallery__card'>
-        <img className='card__img-preview' src={cover} alt={title} title={title} />
-        <figcaption className='card__caption'>{title}</figcaption>
+    <Link to={`/housing/${id}`} className="card__link-housing">
+      <figure className="gallery__card">
+        <img
+          className="card__img-preview"
+          src={cover}
+          alt={title}
+          title={title}
+        />
+        <figcaption className="card__caption">{title}</figcaption>
       </figure>
     </Link>
   )
